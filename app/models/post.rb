5c4a2bbd4d69a63583cb15f01f9416f73fd0bb92
Thermiee <navyblack16@yahoo.com>
2022-10-20 10:26:30 +0100

    Add specs for User, Category, Post models' associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,11 @@
 class Post < ApplicationRecord
-    belongs_to :user
-    belongs_to :category
-  end
-  class User < ApplicationRecord
-    ...
-    has_many :posts, dependent: :destroy       
-  end
-  class Category < ApplicationRecord
-    has_many :posts
-  end
-  
+  belongs_to :user
+  belongs_to :category
+end
+class User < ApplicationRecord
+  ...
+  has_many :posts, dependent: :destroy       
+end
+class Category < ApplicationRecord
+  has_many :posts
+end
