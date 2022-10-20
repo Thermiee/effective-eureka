1a6d495ea6548a18f270b12a1d93de2284f039da
Thermiee <navyblack16@yahoo.com>
2022-10-20 11:01:19 +0100

    Add faker gem - Inside the seeds.rb file create methods to generate   User, Category and Post records inside the development database

--- Gemfile
+++ Gemfile
@@ -85,4 +85,5 @@ group :test do
   gem 'rspec-rails', '~> 3.6'
   gem 'selenium-webdriver'
   gem 'webdrivers'
+  gem 'faker'
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,10 +2,11 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
 end
+
 class User < ApplicationRecord
-  ...
-  has_many :posts, dependent: :destroy       
+  has_many :posts, dependent: :destroy
 end
+
 class Category < ApplicationRecord
   has_many :posts
 end
